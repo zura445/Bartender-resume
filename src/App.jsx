f9eda6e836783589ea07877e32cd1cca9f69a3d0
zura tetrashvili <zuratetra@gmail.com>
2025-01-14 17:23:18 +0400

    add main image

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div className="">sdvcsdfv</div>
-    </>
+    <div
+      className="relative w-full h-screen bg-cover bg-center"
+      style={{ backgroundImage: "url(/image/bar1.jpg)" }}
+    >
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      <div className="relative z-10 flex flex-col items-center justify-center h-full text-white">
+        <h1 className="text-4xl font-bold mb-4">სათაური</h1>
+        <p className="text-lg">ეს</p>
+      </div>
+    </div>
   );
 }
 
