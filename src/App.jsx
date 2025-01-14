bafe39d508b9c7b1e48d4c34a3cf917045786d7b
zura tetrashvili <zuratetra@gmail.com>
2025-01-14 22:34:22 +0400

    change some text

--- src/App.jsx
+++ src/App.jsx
@@ -103,7 +103,7 @@ function App() {
                     <div className="">Bartender</div>
                     <p>Saint-Tropez, Tbilisi</p>
                   </div>
-                  <p>Sep 2024 - Current</p>
+                  <p>Sep 2024 - Present</p>
                   <div>
                     <div className="">Bartender</div>
                     <p>Samkervalo bar, Tbilisi</p>
@@ -123,7 +123,7 @@ function App() {
         <div className="absolute inset-0 bg-black bg-opacity-50"></div>
         <div className="relative flex flex-col justify-center items-center text-white px-4 ">
           <div className="py-10 bg-black opacity-90 flex flex-col items-center px-10 mt-10 rounded-md">
-            <h1 className="text-4xl font-bold mt-4">Personal details</h1>
+            <h1 className="text-4xl font-bold mt-4">Contact details</h1>
             <p className="text-lg mt-4 underline underline-offset-4">
               Email address
             </p>
