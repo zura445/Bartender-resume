c364010d99db16eeae8dd47d8afe203935ead524
zura tetrashvili <zuratetra@gmail.com>
2025-01-24 11:41:43 +0400

    cleaned everything

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,12 +1,10 @@
-import education from "../../src/educationData.json";
+import education from "../data/educationData.json";
 
 function Education() {
-  const { education: educationItems } = education[0];
-
   return (
     <div>
       <h1 className="flex justify-center text-2xl font-semibold">Education</h1>
-      {educationItems.map((educationItem, index) => (
+      {education.map((educationItem, index) => (
         <div key={index} className="grid grid-cols-2 mt-4">
           <p>{educationItem.date}</p>
           <div className="">
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,13 +1,12 @@
-import skillsList from "../skillsData.json";
+import skillsList from "../data/skillsData.json";
 
 function Skills() {
-  const { skillsList: skillsItems } = skillsList[0];
   return (
     <div>
       <h1 className="flex justify-center mt-6 text-2xl font-semibold">
         Skills
       </h1>
-      {skillsItems.map((skillsItem, index) => (
+      {skillsList.map((skillsItem, index) => (
         <div className="grid grid-cols-2 gap-8 mt-4" key={index}>
           <p>{skillsItem.ability}</p>
           <p className="text-center">{skillsItem.quality}</p>
