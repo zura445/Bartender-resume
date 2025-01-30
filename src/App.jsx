0fdf738775933d21dc806bc9eedd6f0b76bd190a
zura tetrashvili <zuratetra@gmail.com>
2025-01-30 23:43:58 +0400

    add new component and finish project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Education from "./components/Education";
 import Skills from "./components/Skills";
 import Employment from "./components/Employment";
+import Contacts from "./components/Contacts";
 
 function App() {
   return (
@@ -17,7 +18,6 @@ function App() {
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 bg-black p-10 opacity-90 rounded-lg">
               <div>
                 <Education />
-
                 <Skills />
               </div>
               <Employment />
@@ -31,24 +31,7 @@ function App() {
         style={{ backgroundImage: "url(/image/bar2.jpg)" }}
       >
         <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-        <div className="relative flex flex-col justify-center items-center text-white px-4 ">
-          <div className="py-10 bg-black opacity-90 flex flex-col items-center px-10 mt-10 rounded-md">
-            <h1 className="text-4xl font-bold mt-4">Contact details</h1>
-            <p className="text-lg mt-4 underline underline-offset-4">
-              Email address
-            </p>
-            <a href="mailto:zuratetra@gmail.com?subject=The%20subject%20of%20the%20mail mt-4">
-              zuratetra@gmail.com
-            </a>
-
-            <p className="text-lg mt-4 underline underline-offset-4">
-              Phone number
-            </p>
-            <a className="cursor-pointer mt-4" href="tel: +995597000540">
-              597 000 540
-            </a>
-          </div>
-        </div>
+        <Contacts />
       </div>
     </div>
   );
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 function Contacts() {
-  return <div>Contacts</div>;
+  return (
+    <div>
+      {" "}
+      <div className="relative flex flex-col justify-center items-center text-white px-4 ">
+        <div className="py-10 bg-black opacity-90 flex flex-col items-center px-10 mt-10 rounded-md">
+          <h1 className="text-4xl font-bold mt-4">Contact details</h1>
+          <p className="text-lg mt-4 underline underline-offset-4">
+            Email address
+          </p>
+          <a href="mailto:zuratetra@gmail.com?subject=The%20subject%20of%20the%20mail mt-4">
+            zuratetra@gmail.com
+          </a>
+
+          <p className="text-lg mt-4 underline underline-offset-4">
+            Phone number
+          </p>
+          <a className="cursor-pointer mt-4" href="tel: +995597000540">
+            597 000 540
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contacts;
